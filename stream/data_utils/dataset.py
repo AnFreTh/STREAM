4d64dfdb4578758495c2af81fe0bc911fed4feff
AFThielmann <antonthielmann@t-online.de>
2024-02-15 13:30:42 +0100

    remove print statements

--- stream/data_utils/dataset.py
+++ stream/data_utils/dataset.py
@@ -36,7 +36,7 @@ class TMDataset(OCTISDataset):
         # Check if the path is relative to the package datasets
         self.name = name
         if dataset_path is None:
-            print("here", dataset_path)
+
             dataset_path = self.get_package_dataset_path(name)
 
         if os.path.exists(dataset_path):
@@ -59,10 +59,8 @@ class TMDataset(OCTISDataset):
     def get_package_dataset_path(name):
         # Get the directory of the current script
         script_dir = os.path.dirname(os.path.abspath(__file__))
-        print(f"script_dix: {script_dir}")
         # Go up one level to the 'MyPackage' directory
         my_package_dir = os.path.dirname(script_dir)
-        print(f"script_dix: {my_package_dir}")
         # Construct the path to the 'preprocessed_datasets' within 'MyPackage'
         dataset_path = os.path.join(my_package_dir, "preprocessed_datasets", name)
         print(f"dataset_path: {dataset_path}")
