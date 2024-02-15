80380624d40fa2cd901f6719fe6d59e8369a4237
AFThielmann <antonthielmann@t-online.de>
2024-02-15 13:16:57 +0100

    change setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,11 @@ setup(
     version="0.1.0",
     packages=find_packages(exclude=["examples", "examples.*", "tests", "tests.*"]),
     install_requires=read_requirements(),
-    include_package_data=True,
+    include_package_data=True,  # This should be True to include files specified in MANIFEST.in
+    package_data={
+        # Adjust the paths to match your dataset and embeddings locations
+        "stream": ["preprocessed_datasets/*", "pre_embedded_datasets/*"],
+    },
     description="A package for expanded topic modeling and metrics",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
