2cfcf46358abb29b7fbc7ee17c678c37eb29aa33
Manish Kumar <manish.kumar@basf.com>
2024-07-21 16:38:43 +0200

    refactor, configuration fix

--- docs/conf.py
+++ docs/conf.py
@@ -33,42 +33,40 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
-    "sphinx.ext.todo",
-    "sphinx.ext.coverage",
     "sphinx.ext.mathjax",
-    "sphinx.ext.ifconfig",
     "sphinx_copybutton",
     "sphinx_togglebutton",
     "nbsphinx",
-    "numpydoc",
+    'myst_nb',
     "IPython.sphinxext.ipython_console_highlighting",
     "IPython.sphinxext.ipython_directive",
-    "myst_parser",
-    "mdinclude",  # custom module
-    "sphinx_rtd_theme",
-    # "pydata_sphinx_theme",
+    # "myst_parser",
+    "mdinclude",
     "sphinx_autodoc_typehints",
 ]
+
 autodoc_mock_imports = [
+    "numpy",
     "lightning",
     "torch",
     "torchmetrics",
     "pytorch_lightning",
-    "numpy",
     "pandas",
     "sklearn",
-    "properscoring",
     "tqdm",
     "transformers",
     "sentence_transformers",
+    "umap",
+    "hdbscan",
+
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_suffix = [".rst", ".md"]
-# source_suffix = ".rst"
+# source_suffix = [".rst", ".ipynb"]
+source_suffix = [".rst", ".ipynb", ".md"]
 
 # The master toctree document.
 master_doc = "index"
@@ -104,9 +102,9 @@ pygments_style = "sphinx"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# 'sphinx_rtd_theme'  # 'furo', 'press', 'pydata_sphinx_theme'
 html_theme = "sphinx_book_theme"
-# html_static_path = ['_static']
+html_static_path = ['_static']
+html_css_files = ['css/stream_theme.css']
 # html_css_files = ['custom.css']
 # html_js_files = ['custom.js']
 
