af2f55f6af08dc568470afe9ec61d10b8a6696bd
AFThielmann <antonthielmann@t-online.de>
2024-07-25 21:13:37 +0200

    add etm

--- stream/models/__init__.py
+++ stream/models/__init__.py
@@ -6,6 +6,7 @@ from .KmeansTM import KmeansTM
 from .lda import LDA
 from .som import SOMTM
 from .WordCluTM import WordCluTM
+from .etm import ETM
 
 __all__ = [
     "BERTopicTM",
@@ -16,4 +17,5 @@ __all__ = [
     "SOMTM",
     "WordCluTM",
     "LDA",
+    "ETM",
 ]
